aa1ee36180086b266844972684acdf229e26728e
LaPlaceDemon1 <miguel.a.rocha.20@gmail.com>
2025-10-20 14:04:43 +0100

    bashs

--- windows_run.sh
+++
@@ -1,3 +0,0 @@
-# Open two PowerShell windows to run messagesapp and client
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd D:\adi\adintproject; .\messagesapp.exe"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd D:\adi\adintproject; .\client.exe"
